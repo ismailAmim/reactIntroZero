803f64e7168c78a2f40680b632ec50a946c467d1
ismailAmim <ismail.amim2010@gmail.com>
2021-08-19 07:23:59 +0100

    create first component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ function App() {
     <div className = "App" >
         <header className = "App-header" >
             <img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <p>Edit <code> src / App.js </code> and save to reload. </
-        p> <h1> This is my first access to react </h1> 
-        <a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React </a> 
+                className = "App-logo"
+                alt = "logo" / >
+            <p>Edit <code> src / App.js </code> and save to reload. </p>
+           <h1> This is my first access to react </h1> 
+           <h2> Hello world react</h2> 
+           <a className = "App-link"
+              href = "https://reactjs.org"
+              target = "_blank"
+              rel = "noopener noreferrer" >
+            Learn React </a> 
         </header> </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <React.StrictMode >
+    {/* <App/> */}
+    <Hello greeting={"hello"+" ninja react"}/> {/* imported  Hello component tag */}
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
